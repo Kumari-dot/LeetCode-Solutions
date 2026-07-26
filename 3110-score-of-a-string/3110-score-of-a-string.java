ce99e7fbe7f5bf17ff812f444066de61ea0afd3e
Thiruveedhula Kumari <thiruveedhulakumari24@gmail.com>
2026-07-26 14:57:48 +0530

    Time: 1 ms (98.53%), Space: 43.2 MB (72.34%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int scoreOfString(String s) {
+        char[] a=s.toCharArray();
+        int sum=0;
+        for(int i=0;i<a.length-1;i++){
+            sum+=Math.abs(a[i]-a[i+1]);
+        }
+        return sum;
+        
+    }
+}
